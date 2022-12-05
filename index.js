a80a15a5a7ac168a57a7f05f56f382d371ec3bfb
Lucas Martinez Asensio <martinezasensiolucas@gmail.com>
2022-12-04 22:41:56 -0300

    main route html edited

--- index.js
+++ index.js
@@ -29,15 +29,42 @@ const options = {
 app.use(cors(options));
 
 app.get('/', (req, res) => {
-  res.send(`hi im a server made with express
+  res.send(`
+  <head><style>
+  body {
+    background-color: rgb(41,42,43);
+    color: rgb(243,243,230);
+    font-family: verdana
+  }
+  </style></head>
+  <h3>Hi! This is an API RESTful developed using Express.</h3>
+  Other libraries used: <br>
+  Faker <br>
+  Boom <br>
+  Joi <br>
+  Cors <br> <br>
 
-  These are my main routes:
-    /api/v1/products
-    /api/v1/categories
-    /api/v1/users
 
-  To see each route content:
-    add to previous routes /id
+
+
+  These are my main routes: <br>
+  /api/v1/products <br>
+  /api/v1/categories <br>
+  /api/v1/users <br><br>
+
+  To filter each route content:<br>
+  add to previous routes /id<br><br><br>
+
+  Implemententing Separation of Concerns and Clean Architecture principles: <br>
+  Creation and Management of Entities,<br>
+  Services and business logic,<br>
+  Routing and Middlewares <br><br>
+
+  Access Management <br>
+  Data Validation <br>
+  Error Management specifing correct http status codes.<br> <br>
+
+  Deploy with Railway
 
   `);
 });
